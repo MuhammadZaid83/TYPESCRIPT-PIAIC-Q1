f84998f97b44a5fec787d7ca6c20cdfd91ded2d5
ZAID <muahmmadzaid83@gmail.com>
2023-11-08 08:12:12 +0500

    first commit

---
+++ Assignment 2/assignment2.js
@@ -0,0 +1,43 @@
+"use strict";
+console.log("ZAid");
+// Write a program to convert the temperature from Celsius to Fahrenheit and vice verse.
+var num = 41;
+var temp = num / (4 + 2) - 5;
+console.log("calculates is", temp);
+var temp = num + 21 * (11 * 3);
+console.log("calculates is", temp);
+//Write a program that calculates the percentage
+var obtainedMarks = 475;
+var totalMarks = 505;
+var result = obtainedMarks / totalMarks * 100;
+console.log("marks percentage is", result);
+// Write a program that checks if a given number is positive, negative, or zero.
+var num = 5;
+if (num > 0) {
+    console.log("positive.");
+}
+else if (num < 0) {
+    console.log("negative.");
+}
+else {
+    console.log("zero.");
+}
+//Write a program that checks if a given number is even or odd
+var num = 5;
+if (num % 2 == 0) {
+    console.log("number is even.");
+}
+else {
+    console.log("number is odd.");
+}
+//Write a program that determines if a person is eligible to vote based on their age.
+var age = 18;
+if (age >= 18) {
+    console.log("eligible to vote.");
+}
+else {
+    console.log("not eligible to vote.");
+}
+//Write a program that calculates the result of a mathematical expression.	((10 + 5) * 3 - 2) / (4 % 3)  - 7)
+var result = ((10 + 5) * 3 - 2) / (4 % 3) - 7;
+console.log(result);
---
+++ Assignment 5/index.ts
@@ -0,0 +1,100 @@
+// Assignment for the week:
+
+//  - Develop a program that calculates and prints the sum of the first n even numbers using a for loop.
+
+// function sumOfFirstNEvenNumbers(n: number): number {
+//   let sum = 0;
+
+//   for (let i = 1; i <= n; i++) {
+//     // Calculate the i-th even number (2 times i)
+//     const evenNumber = 2 * i;
+//     sum += evenNumber;
+//   }
+
+//   return sum;
+// }
+
+// const n = 5; // You can change this to the desired value of n
+// const result = sumOfFirstNEvenNumbers(n);
+// console.log("The sum of the first",n ,"even numbers is:", result);
+
+
+
+//  - Implement a program that uses a for loop to iterate through an array of numbers and print only the even numbers.
+
+// function printEvenNumbers(numbers: number[]): void {
+//     for (let i = 0; i < numbers.length; i++) {
+//        if (numbers[i] % 2 === 0) {
+//          console.log(numbers[i]);
+//        }
+//     }
+//    }
+   
+//    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // You can change this to the desired array of numbers
+//    printEvenNumbers(numbers);
+
+//  - Implement a program that uses a loop to iterate through an array of numbers and remove all the even numbers from them and just leave the odd ones
+
+// function removeEvenNumbers(numbers: number[]): number[] {
+//     const oddNumbers: number[] = [];
+
+//     for (let i = 0; i < numbers.length; i++) {
+//        if (numbers[i] % 2 !== 0) {
+//          oddNumbers.push(numbers[i]);
+//        }
+//     }
+
+//     return oddNumbers;
+// }
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // You can change this to the desired array of numbers
+// const result = removeEvenNumbers(numbers);
+// console.log("The array after removing even numbers is:", result);
+
+
+//  - Write a program that defines a function to calculate the area of a circle. The function should take the radius as input and return the calculated area.
+
+// function areaOfCircle(radius:number): number {
+//     let pi=3.1416;
+//     let formulaofarea=pi*radius*radius;
+//     return formulaofarea;
+// }
+// let radius=5.6;
+// let result=areaOfCircle(radius);
+// console.log("Area of cirlce is",result);
+
+//  - Develop a program that reads a list of grades and uses the splice method to remove failing grades (below 50) from the array.
+
+// function removeFailingGrades(grades: number[]): number[] {
+//     const passingGrades: number[] = [];
+
+//     for (let i = 0; i < grades.length; i++) {
+//         if (grades[i] >= 50) {
+//             passingGrades.push(grades[i]);
+//         }
+//     }
+
+//     return passingGrades;
+// }
+
+// const grades = [85, 90, 45, 70, 55, 80, 60, 35, 75, 50]; // You can change this to the desired array of grades
+// const result = removeFailingGrades(grades);
+// console.log("The array after removing failing grades is:", result);
+
+//  - Write a program that uses a function to find the largest element in an array of numbers
+
+// function findLargestElement(numbers: number[]): number {
+//     let largestElement = numbers[0];
+
+//     for (let i = 1; i < numbers.length; i++) {
+//         if (numbers[i] > largestElement) {
+//             largestElement = numbers[i];
+//         }
+//     }
+
+//     return largestElement;
+// }
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // You can change this to the desired array of numbers
+// const result = findLargestElement(numbers);
+// console.log("The largest element in the array is:", result);

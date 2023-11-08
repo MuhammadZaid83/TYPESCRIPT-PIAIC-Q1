b1dfd57fb6bb9a6e5fe687c0f1290f1bcd922689
Muhammad Zaid <zaidgoraya018@gmail.com>
2023-11-08 08:19:30 +0500

    Add files via upload

---
+++ First Class Assignment/Class 01/index.ts
@@ -0,0 +1,24 @@
+var message:string = ("Hey Zaid,I have started Learning TYPESCRIPT");
+console.log(message);
+var myName:string = ("My Name is Muhammad Zaid");
+console.log(myName);
+var num1:number = 1;
+var num2:number = 2;
+var num3:number = 3;
+var num4:number = 4;
+var num5:number = 5;
+var num6:number = 6;
+var num7:number = 7;
+var num8:number = 8;
+var num9:number = 9;
+var num10:number = 10;
+var sum =num1+num2+num3+num4+num5+num6+num7+num8+num9+num10;
+console.log(sum);
+var subtract =num1-num2-num3-num4-num5-num6-num7-num8-num9-num10;
+console.log(subtract);
+var multiply =num1*num2*num3*num4*num5*num6*num7*num8*num9*num10;
+console.log(multiply);
+var divide =num5/num9;
+console.log(divide);
+
+
